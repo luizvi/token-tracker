aa3105888ac41969d1e87a9a8375c5c3d3fd9735
luizvi <70661459+luizvi@users.noreply.github.com>
2026-05-02 18:30:31 -0300

    feat(db): exporta API pública do pacote via barrel index

---
+++ packages/db/src/index.ts
@@ -0,0 +1,16 @@
+export { createClient, type DbClient, type ClientHandles } from "./client.js";
+export { runMigrations } from "./migrate.js";
+export { seedDatabase } from "./seed.js";
+
+export * as schema from "./schema.js";
+
+export * from "./queries/clients.js";
+export * from "./queries/projects.js";
+export * from "./queries/sessions.js";
+export * from "./queries/tasks.js";
+export * from "./queries/events.js";
+export * from "./queries/tags.js";
+export * from "./queries/settings.js";
+export * from "./queries/pricing.js";
+export * from "./queries/currency.js";
+export * from "./queries/diagnostics.js";
